2480abf88ad2e93ca22b0c784507bb4505acf001
Brandon Herford <brandonherford@Mac.fios-router.home>
2025-10-15 19:11:09 -0400

    Add exercises and chapters

---
+++ python_book/04_variables/exercises/01_exercise.py
@@ -0,0 +1,13 @@
+# index - this is idiomatic because it does not break convention rules, but it is very generic, and may lead to confusion because it could mean many different things throught a program. (good)
+
+# CatName - this is pascal case, and is non-idomatic because it follows an illegal convention for naming, variable names that start with upper case should be reserved for class names or Constants. (good)
+
+# lazy_dog - is snake case, and is idomatic because it follows legal python naming convention. (good)
+
+# quick_Fox - this is non-idiomatic because it is mixing two conventions together, snake case and camel case (good)
+
+# 1stCharacter - variables should not start with numbers, this is non-idiomatic, but is legal. (illegal after code review)
+
+# operand2 - this is an ilegal variable name, variables cannot end with a number. (can end variable names with numbers, idiomatic)
+
+# π - this is a symbol, and is ilegal. (can use unicode though non-idiomatic)
